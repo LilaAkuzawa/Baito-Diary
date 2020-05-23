678bc4063ad00110c19ef490c59ba41faedc5f00
LilaAkuzawa <sk8erboi.0522ra@gmail.com>
2020-05-23 15:45:48 +0900

    Initial Commit

---
+++ Albeit Diary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Albeit Diary
+//
+//  Created by 阿久沢莉良 on 2020/05/23.
+//  Copyright © 2020 Chai Akuzawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
